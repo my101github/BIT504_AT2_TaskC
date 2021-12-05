00e6ad75e479a2adce10cdf0a0c8258057e558b3
my101github <91169104+my101github@users.noreply.github.com>
2021-12-05 16:29:27 +1300

    Update PongPanel.java
    - Changed the background colour from 'BLACK' to 'GRAY'.
    - Changed a ball movement speed from '1' to '5' to move the ball faster.

--- PongPanel.java
+++ PongPanel.java
@@ -14,9 +14,9 @@ import javax.swing.Timer;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
-	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static Color BACKGROUND_COLOUR = Color.GRAY;
 	private final static int TIMER_DELAY = 5;
-	private final static int BALL_MOVEMENT_SPEED = 1;
+	private final static int BALL_MOVEMENT_SPEED = 5;
 	private final static int POINTS_TO_WIN = 3;
 	private final static int SCORE_TEXT_X = 100;
 	private final static int SCORE_TEXT_Y = 100;
